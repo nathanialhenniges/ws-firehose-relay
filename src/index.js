41faa4689fa439abbd4f5d4dba68e0b6921ba7f7
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-07-24 21:43:26 -0500

    Temp fix.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ const fs = require('fs');
 if(process.env.SSL === 'true') {
 const privateKey = fs.readFileSync(process.env.KEY, 'utf8');
 const certificate = fs.readFileSync(process.env.CERT, 'utf8');
-}
+
 const credentials = { key: privateKey, cert: certificate };
 const https = require('https');
 
 const httpsServer = https.createServer(credentials);
 httpsServer.listen(process.env.PORT);
+}
 
-const WebSocket = require('ws');
 const wss = new WebSocket.Server({
   server: httpsServer
 });
+}
 
+const wss = new WebSocket.Server({ port: process.env.PORT })
+const WebSocket = require('ws');
 require("./firehose")(wss);
 
 
