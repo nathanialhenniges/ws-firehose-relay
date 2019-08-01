a5aa93d140883b272cedc81b7aa104d6a24150a5
Nathan Henniges <demonwolf@demonwolfdev.com>
2019-08-01 17:33:00 -0500

    fixed

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 const fs = require('fs');
 const WebSocket = require('ws');
+let wss;
+if (process.env.SSL === 'true') {
+  const privateKey = fs.readFileSync(process.env.KEY, 'utf8');
+  const certificate = fs.readFileSync(process.env.CERT, 'utf8');
 
-if(process.env.SSL === 'true') {
-const privateKey = fs.readFileSync(process.env.KEY, 'utf8');
-const certificate = fs.readFileSync(process.env.CERT, 'utf8');
+  const credentials = { key: privateKey, cert: certificate };
+  const https = require('https');
 
-const credentials = { key: privateKey, cert: certificate };
-const https = require('https');
+  const httpsServer = https.createServer(credentials);
+  httpsServer.listen(process.env.PORT);
 
-const httpsServer = https.createServer(credentials);
-httpsServer.listen(process.env.PORT);
-
-
-const wss = new WebSocket.Server({
-  server: httpsServer
-});
+  wss = new WebSocket.Server({
+    server: httpsServer
+  });
+} else {
+  wss = new WebSocket.Server({ port: process.env.PORT })
 }
 
-const wss = new WebSocket.Server({ port: process.env.PORT })
 
 require("./firehose")(wss);
 
